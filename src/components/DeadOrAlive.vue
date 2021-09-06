ec89a8871c5373c2264560dad524c12642fa3268
Samuel Morrissey <samuel.morrissey@outlook.com>
2021-09-06 18:37:02 -0300

    Chip icon fix 2

--- src/components/DeadOrAlive.vue
+++ src/components/DeadOrAlive.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-chip :color="color" icon='fa fa-heart' text-color="white">{{ text }}</q-chip>
+  <q-chip :color="color" :icon='icon' text-color="white">{{ text }}</q-chip>
 </template>
 
 <script lang="ts">
